0d0368837b2d5753aa7c607eafbe3dc36e3a284d
arthas <arthastheking113@gmail.com>
2021-03-17 02:25:58 -0500

    fix a little bit

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -33,7 +33,7 @@ namespace BlogTest.Areas.Identity.Pages.Account
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return RedirectToAction("Index","Home");
             }
             else
             {
