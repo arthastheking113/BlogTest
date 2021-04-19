82694931295c2ae1fdc29a1735a6ed8c55c0e152
arthas <arthastheking113@gmail.com>
2021-04-19 15:15:03 -0500

    fix app crash

--- Startup.cs
+++ Startup.cs
@@ -140,7 +140,7 @@ namespace BlogTest
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            } 
+            }  
             app.UseCors("DefaultPolicy");
             app.UseSwagger();
             app.UseSwaggerUI(c =>
